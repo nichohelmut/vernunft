592e0c6b83eec78c4f86e4331655925a0ccf748f
nichohelmut <nicholasutikal@gmail.com>
2018-11-20 17:55:48 +0100

    adds views, routes, controllers and methods for basic UX

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!
 
   def home
+  end
+
+  def wir
+  end
+
+  def carta
+  end
+
+  def join
+  end
+
+  def testimonial
   end
 end
